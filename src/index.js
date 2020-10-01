a15c10d9faae1d9ae1bb46a1361612da8270caf6
Angel Martinez <angel.codes139@gmail.com>
2020-09-30 21:22:35 -0500

    :lipstick: styles(root): adding tailwind styles to the app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// TailwindCSS
+import './styles.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
