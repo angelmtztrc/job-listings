82fa0fa532bbaf3f1426ea0b2f4bdac84aae4d29
Angel Martinez <angel.codes139@gmail.com>
2020-10-01 18:39:18 -0500

    :sparkles: feature(app): adding my tag

--- src/App.js
+++ src/App.js
@@ -10,11 +10,22 @@ import JobsList from './components/JobsList';
 function App() {
   return (
     <JobsProvider>
-      <div>
+      <div className="relative">
         <Header />
         <main className="mt-20">
           <JobsList />
         </main>
+        <div className="absolute top-0 left-0 w-full mt-2">
+          <p className="text-xs text-center text-cyan-very-light-grayish font-bold">
+            Made with ❤ by{' '}
+            <a
+              className="hover:underline"
+              href="https://github.com/angel-codes/job-listings"
+            >
+              Angel Martinez
+            </a>
+          </p>
+        </div>
       </div>
     </JobsProvider>
   );
