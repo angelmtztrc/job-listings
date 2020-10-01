ed430fdbf24bf52898b0b158d81b8ac25a6ba873
Angel Martinez <angel.codes139@gmail.com>
2020-10-01 17:19:31 -0500

    :sparkles: feature(filter): show filters

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import React, { useContext } from 'react';
 import { JobsContext } from '../context/jobs/JobsProvider';
 
 const Header = () => {
+  // Access to the context values
   const JobsState = useContext(JobsContext);
   const { filters } = JobsState;
 
@@ -17,27 +18,29 @@ const Header = () => {
           {filters.length >= 1 ? (
             <div className="bg-white px-8 py-4 shadow-md rounded-md flex justify-between items-center">
               <ul className="flex">
-                <li className="bg-cyan-light-gryish p-3 pr-12 rounded-md relative">
-                  <p className="text-cyan-primary leading-none font-semibold mr-2">
-                    Front-End
-                  </p>
-                  <button className="absolute top-0 right-0 bg-cyan-primary hover:bg-cyan-very-dark-grayish h-full w-10 flex justify-center items-center rounded-r-md transition-colors ease-out duration-300">
-                    <svg
-                      className="w-5 h-5 text-white"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="3"
-                        d="M6 18L18 6M6 6l12 12"
-                      ></path>
-                    </svg>
-                  </button>
-                </li>
+                {filters.map(filter => (
+                  <li className="bg-cyan-light-gryish p-3 pr-12 rounded-md relative mr-4">
+                    <p className="text-cyan-primary leading-none font-semibold mr-2">
+                      {filter}
+                    </p>
+                    <button className="absolute top-0 right-0 bg-cyan-primary hover:bg-cyan-very-dark-grayish h-full w-10 flex justify-center items-center rounded-r-md transition-colors ease-out duration-300">
+                      <svg
+                        className="w-5 h-5 text-white"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="3"
+                          d="M6 18L18 6M6 6l12 12"
+                        ></path>
+                      </svg>
+                    </button>
+                  </li>
+                ))}
               </ul>
               <button className="text-cyan-primary font-semibold hover:underline">
                 Clear
