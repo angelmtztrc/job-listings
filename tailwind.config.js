6ba32077f4a57b568063367ff92397af79249814
Angel Martinez <angel.codes139@gmail.com>
2020-10-01 13:42:39 -0500

    :wrench: config(tailwindcss): adding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
+    purgeLayersByDefault: true
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyan-primary': '#5ba4a4',
+        'cyan-light-gryish': '#eef6f6',
+        'cyan-very-light-grayish': '#effafa',
+        'cyan-dark-grayish': '#7b8e8e',
+        'cyan-very-dark-grayish': '#2c3a3a'
+      }
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
