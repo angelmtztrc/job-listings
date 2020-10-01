6d84e0a5adc8e3cab83b84c13f2c007b421cdcc7
Angel Martinez <angel.codes139@gmail.com>
2020-10-01 16:32:54 -0500

    :sparkles: feature(filter): hide filter when no values

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+// Context
+import { JobsContext } from '../context/jobs/JobsProvider';
 
 const Header = () => {
+  const JobsState = useContext(JobsContext);
+  const { filters } = JobsState;
+
   return (
     <header
       id="header"
@@ -8,34 +14,36 @@ const Header = () => {
     >
       <div style={{ bottom: -40 }} className="absolute bottom-0 left-0 w-full">
         <div className="container mx-auto ">
-          <div className="bg-white px-8 py-4 shadow-md rounded-md flex justify-between items-center">
-            <ul className="flex">
-              <li className="bg-cyan-light-gryish p-3 pr-12 rounded-md relative">
-                <p className="text-cyan-primary leading-none font-semibold mr-2">
-                  Front-End
-                </p>
-                <button className="absolute top-0 right-0 bg-cyan-primary hover:bg-cyan-very-dark-grayish h-full w-10 flex justify-center items-center rounded-r-md transition-colors ease-out duration-300">
-                  <svg
-                    className="w-5 h-5 text-white"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="3"
-                      d="M6 18L18 6M6 6l12 12"
-                    ></path>
-                  </svg>
-                </button>
-              </li>
-            </ul>
-            <button className="text-cyan-primary font-semibold hover:underline">
-              Clear
-            </button>
-          </div>
+          {filters.length >= 1 ? (
+            <div className="bg-white px-8 py-4 shadow-md rounded-md flex justify-between items-center">
+              <ul className="flex">
+                <li className="bg-cyan-light-gryish p-3 pr-12 rounded-md relative">
+                  <p className="text-cyan-primary leading-none font-semibold mr-2">
+                    Front-End
+                  </p>
+                  <button className="absolute top-0 right-0 bg-cyan-primary hover:bg-cyan-very-dark-grayish h-full w-10 flex justify-center items-center rounded-r-md transition-colors ease-out duration-300">
+                    <svg
+                      className="w-5 h-5 text-white"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="3"
+                        d="M6 18L18 6M6 6l12 12"
+                      ></path>
+                    </svg>
+                  </button>
+                </li>
+              </ul>
+              <button className="text-cyan-primary font-semibold hover:underline">
+                Clear
+              </button>
+            </div>
+          ) : null}
         </div>
       </div>
     </header>
