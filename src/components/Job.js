b4e2dddebb553e2a284457d601c0698e7e070538
Angel Martinez <angel.codes139@gmail.com>
2020-10-01 17:07:40 -0500

    :sparkles: feature(jobs): show jobs info

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,49 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Job = () => {
+const Job = ({ job }) => {
+  const { role, level, languages, tools, logo } = job;
+
+  // Create an array with all categories
+  const categories = [role, level, ...tools, ...languages];
+
   return (
-    <li className="border-l-4 border-cyan-primary p-8 shadow-md rounded-md">
+    <li className="border-l-4 border-cyan-primary p-8 shadow-md rounded-md mb-5">
       <div className="flex justify-between items-center">
         <div className="flex">
-          <div className="h-24 w-24 bg-gray-500 rounded-full mr-5"></div>
+          <div
+            style={{
+              backgroundImage: `url(${require(`./../images/${logo}`)})`
+            }}
+            className="h-24 w-24 rounded-full mr-5 bg-cover bg-center"
+          ></div>
           <div>
             <section className="flex items-center mb-3">
               <p className="text-cyan-primary font-semibold mr-5">
-                T-Rex Codes
+                {job.company}
               </p>
-              <span className="text-xs mr-3 px-3 pt-3 pb-2 bg-cyan-primary leading-none rounded-full text-white font-semibold">
-                <p>NEW!</p>
-              </span>
-              <span className="text-xs px-3 pt-3 pb-2 bg-cyan-very-dark-grayish leading-none rounded-full text-white font-semibold">
-                <p>FEATURED</p>
-              </span>
+              {job.new ? (
+                <span className="text-xs mr-3 px-3 pt-3 pb-2 bg-cyan-primary leading-none rounded-full text-white font-semibold">
+                  <p>NEW!</p>
+                </span>
+              ) : null}
+              {job.featured ? (
+                <span className="text-xs px-3 pt-3 pb-2 bg-cyan-very-dark-grayish leading-none rounded-full text-white font-semibold">
+                  <p>FEATURED</p>
+                </span>
+              ) : null}
             </section>
             <h2 className="text-xl text-cyan-very-dark-grayish hover:text-cyan-primary font-bold cursor-pointer transition-colors ease-out duration-300">
-              Senio Frontend Developer
+              {job.position}
             </h2>
             <section id="extra-info" className="text-cyan-dark-grayish mt-3">
-              <span>1d ago</span>
-              <span>Full Time</span>
-              <span>USA only</span>
+              <span>{job.postedAt}</span>
+              <span>{job.contract}</span>
+              <span>{job.location}</span>
             </section>
           </div>
         </div>
         {/* end - information of the job */}
         <ul className="flex">
-          <li className="mr-2">
-            <button className="px-4 py-2 focus:outline-none bg-cyan-light-gryish hover:bg-cyan-primary text-cyan-primary hover:text-cyan-light-gryish font-semibold rounded-md transition-colors ease-in-out duration-500">
-              Senior
-            </button>
-          </li>
-          <li className="mr-2">
-            <button className="px-4 py-2 focus:outline-none bg-cyan-light-gryish hover:bg-cyan-primary text-cyan-primary hover:text-cyan-light-gryish font-semibold rounded-md transition-colors ease-in-out duration-500">
-              Frontend
-            </button>
-          </li>
+          {categories.map((category, index) => (
+            <li key={index} className="mr-2">
+              <button className="px-4 py-2 focus:outline-none bg-cyan-light-gryish hover:bg-cyan-primary text-cyan-primary hover:text-cyan-light-gryish font-semibold rounded-md transition-colors ease-in-out duration-500">
+                {category}
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </li>
   );
+};
+
+Job.propTypes = {
+  job: PropTypes.object.isRequired
 };
 
 export default Job;
--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+// Context
+import { JobsContext } from '../context/jobs/JobsProvider';
+
+// Components
 import Job from './Job';
 
 const JobsList = () => {
+  const JobsState = useContext(JobsContext);
+  const { jobs } = JobsState;
+
   return (
     <ul className="flex flex-col container mx-auto">
-      <Job />
+      {jobs.map(job => (
+        <Job key={job.id} job={job} />
+      ))}
     </ul>
   );
 };
